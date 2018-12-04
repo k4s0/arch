cdaf4c8b74f4301f9eff92b7a81711104e6f0a5e
Casini Lorenzo <casinilorenzo@protonmail.com>
2018-12-04 09:52:49 +0100

    update arch-helper
    added README.md with usefull procedures to set up you're archlinux configuration

--- helper.sh
+++ helper.sh
@@ -12,7 +12,7 @@ sleep 1
 echo Checking script dependencies...
 sleep 1
 
-command -v aurman >/dev/null 2>&1 || { echo >&2 "I require aurman but it's not installed.Aborting."; exit 1;}
+command -v yay >/dev/null 2>&1 || { echo >&2 "I require yay but it's not installed.Aborting."; exit 1;}
 
 #checking root privileges
 if [[ $UID == 0 ]]; then
@@ -53,7 +53,7 @@ clear
 sleep 1
 echo "Updating AUR repo database..."
 sleep 1
-aurman -Syyu
+yay -Syu
 
 #printing finish info
 clear
