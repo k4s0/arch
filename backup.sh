b39b379cd4ba90287c449f259b86c2766124e05b
Lroenzo <casinilorenzo@pm.me>
2020-01-12 18:05:51 +0100

    update bkp-ignore-list.txt, fix backup.sh

--- backup.sh
+++ backup.sh
@@ -5,4 +5,4 @@ sleep 3
 clear
 echo SAPHIRE:  starting network backup on RASPO
 sleep 3
-rsync -vrahe ssh --delete --exclude=.[^.] --exclude=*.avi ~/ pi@192.168.1.100:~/bkp_saphire_notebook
+rsync -vrahe ssh --delete --exclude-from=bkp-ignore-list.txt ~/ pi@192.168.1.100:~/bkp_saphire_notebook
